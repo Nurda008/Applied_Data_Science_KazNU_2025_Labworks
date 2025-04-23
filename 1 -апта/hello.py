f7e38faf1dc688f915cb84797939aa8604b984c6
Nurda <amannurdaulet14@gmail.com>
2025-04-23 21:57:08 +0500

    Зертханалық жұмыстар мен деректер

---
+++ 1 -апта/hello.py
@@ -0,0 +1,12 @@
+
+print("Hello, World!")
+
+name = input("Атыңыз кім? ")
+
+print(f"Қош келдіңіз, {name}!")
+
+num1 = float(input("Бірінші санды енгізіңіз: "))
+num2 = float(input("Екінші санды енгізіңіз: "))
+
+sum_result = num1 + num2
+print(f"Екі санның қосындысы: {sum_result}")
